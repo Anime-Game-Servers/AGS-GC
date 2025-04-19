ed5adff6e67a98f4ebe84cc5dce851a6bff9c3d5
hartie95 <mail@hartie95.de>
2025-04-19 16:02:31 +0200

    [Refactoring] remove unused import

--- src/main/java/emu/grasscutter/game/ability/actions/ActionChangeTag.java
+++ src/main/java/emu/grasscutter/game/ability/actions/ActionChangeTag.java
@@ -3,7 +3,6 @@ package emu.grasscutter.game.ability.actions;
 import emu.grasscutter.data.binout.AbilityModifier.AbilityModifierAction;
 import emu.grasscutter.game.ability.Ability;
 import emu.grasscutter.game.entity.GameEntity;
-import emu.grasscutter.server.packet.send.PacketEntityTagChangeNotify;
 
 @AbilityAction(AbilityModifierAction.Type.ChangeTag)
 public class ActionChangeTag extends AbilityActionHandler {
