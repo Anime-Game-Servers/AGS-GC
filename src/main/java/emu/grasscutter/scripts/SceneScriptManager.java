scooterboo/Grasscutter-Quests

Make region entering/leaving symmetrical.

src/main/java/emu/grasscutter/scripts/SceneScriptManager.java
@@ -9,6 +9,7 @@ import emu.grasscutter.data.server.Grid;
 import emu.grasscutter.database.DatabaseHelper;
 import emu.grasscutter.game.entity.*;
 import emu.grasscutter.game.entity.gadget.platform.BaseRoute;
+import emu.grasscutter.game.props.EntityType;
 import emu.grasscutter.game.quest.GameQuest;
 import emu.grasscutter.game.quest.QuestGroupSuite;
 import emu.grasscutter.game.quest.enums.QuestContent;
@@ -571,11 +572,13 @@ public class SceneScriptManager {
             val metaRegion = region.getMetaRegion();
 
             getScene().getEntities().values().stream()
+                .filter(e -> e.getEntityType() == EntityType.Avatar)
                 .filter(e -> metaRegion.contains(e.getPosition()) && !region.getEntities().contains(e))
                 .forEach(region::addEntity);
 
-            region.getEntities().stream()
-                .filter(e -> !metaRegion.contains(e.getPosition()))
+            getScene().getEntities().values().stream()
+                .filter(e -> e.getEntityType() == EntityType.Avatar)
+                .filter(e -> !metaRegion.contains(e.getPosition()) && !region.getNotContainEntities().contains(e))
                 .forEach(region::removeEntity);
 
             // call enter region events for new entities

Anime-Game-Servers/AGS-GC

src/main/java/emu/grasscutter/scripts/SceneScriptManager.java
@@ -11,6 +11,7 @@ import emu.grasscutter.game.entity.create_config.CreateGadgetEntityConfig;
 import emu.grasscutter.game.entity.create_config.CreateMonsterEntityConfig;
 import emu.grasscutter.game.entity.create_config.CreateNpcEntityConfig;
 import emu.grasscutter.game.entity.create_config.CreateRegionEntityConfig;
+import emu.grasscutter.game.props.EntityType;
 import emu.grasscutter.game.quest.GameQuest;
 import emu.grasscutter.game.quest.QuestGroupSuite;
 import emu.grasscutter.game.quest.enums.QuestCond;
@@ -590,11 +591,13 @@ public class SceneScriptManager {
 
         for (var region : this.regions.values()) {
             getScene().getEntities().values().stream()
+                .filter(e -> e.getEntityType() == EntityType.Avatar)
                 .filter(e -> region.isPosInRegion(e.getPosition()) && !region.getEntities().contains(e))
                 .forEach(region::addEntity);
 
-            region.getEntities().stream()
-                .filter(e -> !region.isPosInRegion(e.getPosition()))
+            getScene().getEntities().values().stream()
+                .filter(e -> e.getEntityType() == EntityType.Avatar)
+                .filter(e -> !region.isPosInRegion(e.getPosition()) && !region.getNotContainEntities().contains(e))
                 .forEach(region::removeEntity);
 
             // call enter region events for new entities