496ea5cb70f890015bb89ec9ac8d3c6ec86a75df
scooterboo <lewasite@yahoo.com>
2025-06-15 00:25:36 -0700

    [Documentation] Add Javadoc

--- src/main/java/emu/grasscutter/game/managers/dailyquest/DailyTaskManager.java
+++ src/main/java/emu/grasscutter/game/managers/dailyquest/DailyTaskManager.java
@@ -22,6 +22,12 @@ import java.util.*;
 /**
  * Handles logic related to the daily quest system.
  * This system handles daily quest variables, packets, and randomization.
+ * TODO:
+ *  Implement commission rewards for finishing all tasks in a day. (What triggers it?)
+ *  Randomization should choose only one task from each pool
+ *  the pools should prefer the dailies found in CityTaskOpenExcelConfigData.json first.
+ *  Dailies have a whole questTrigger-like system for linking them. See DailyTaskData and DailyTaskExcelConfig
+ *  Removing old currentTasks from the quest list is hacky and requires a relog to not look glitchy. Is this the best way?
  */
 @Entity
 public class DailyTaskManager extends BasePlayerDataManager {
@@ -30,10 +36,13 @@ public class DailyTaskManager extends BasePlayerDataManager {
     @Getter private List<Integer> unlockedCities;
     @Getter @Setter private int cityFilter;
     @Getter private int taskLevel;
-    @Getter @Setter private int LegendaryKeyDailyTasks;
+    @Getter @Setter private int legendaryKeyDailyTasks;
     private Map<Integer, List<Integer>> taskVars;
 
-    // For Morphia Only
+    /**
+     * Morphia use only.
+     * Do not use
+     */
     public DailyTaskManager() {
         super();
         currentTasks = new ArrayList<>();
@@ -41,7 +50,7 @@ public class DailyTaskManager extends BasePlayerDataManager {
         unlockedCities = new ArrayList<>();
         taskLevel = 1;
         cityFilter = 0;
-        LegendaryKeyDailyTasks = 0;
+        legendaryKeyDailyTasks = 0;
         taskVars = new HashMap<>();
     }
 
@@ -52,14 +61,22 @@ public class DailyTaskManager extends BasePlayerDataManager {
         unlockedCities = new ArrayList<>();
         taskLevel = 1;
         cityFilter = 0;
-        LegendaryKeyDailyTasks = 0;
+        legendaryKeyDailyTasks = 0;
         taskVars = new HashMap<>();
     }
 
+    /**
+     * Updates the task level to match the player's level.
+     * See DailyTaskLevelExcelConfigData.json
+     */
     public void updateTaskLevel() {
-        this.taskLevel = 1 + ((this.player.getLevel() - 1) / 5); //see DailyTaskLevelExcelConfigData.json
+        this.taskLevel = 1 + ((this.player.getLevel() - 1) / 5);
     }
 
+    /**
+     * Changes the daily tasks into new tasks.
+     * Cleans up the old tasks.
+     */
     public void randomizeTasks() {
         //clear finished tasks
         finishedCurrentTasks.clear();
@@ -82,9 +99,9 @@ public class DailyTaskManager extends BasePlayerDataManager {
         //shuffle tasks
         Collections.shuffle(taskList);
 
-        //limit tasks to at most 5 tasks
-        if (taskList.size() >= 5) {
-            this.currentTasks = taskList.subList(0, 5).stream().map(DailyTaskData::getId).toList();
+        //limit tasks to at most 4 tasks
+        if (taskList.size() >= 4) {
+            this.currentTasks = taskList.subList(0, 4).stream().map(DailyTaskData::getId).toList();
         } else {
             this.currentTasks = taskList.stream().map(DailyTaskData::getId).toList();
         }
@@ -94,15 +111,27 @@ public class DailyTaskManager extends BasePlayerDataManager {
             this.player.getQuestManager().queueEvent(QuestCond.QUEST_COND_DAILY_TASK_START, task));
     }
 
+    /**
+     * Sends DailyTaskDataNotify and TaskVarNotify.
+     * These happen on player login.
+     */
     public void onPlayerLogin() {
         this.player.sendPacket(new PacketDailyTaskDataNotify(this.player));
         this.player.sendPacket(new PacketTaskVarNotify(this.player));
     }
 
+    /**
+     * Returns the scorePreviewRewardId found in DailyTaskLevelExcelConfigData for the player's current task level
+     */
     public int getScoreRewardId() {
         return GameData.getDailyTaskLevelDataMap().get(this.taskLevel).getScorePreviewRewardId();
     }
 
+    /**
+     * Checks if a given questId should unlock daily tasks for any cities.
+     *
+     * @param subQuestId The quest that should be checked against CityTaskOpenExcelConfigData.
+     */
     public void checkForCityUnlock(int subQuestId) {
         GameData.getCityTaskOpenDataMap().values().forEach(city -> {
             if (city.getQuestId() == subQuestId && !unlockedCities.contains(city.getCityId())) {
@@ -112,7 +141,11 @@ public class DailyTaskManager extends BasePlayerDataManager {
         });
     }
 
-    //outputs taskVars as a proto as seen in DailyTaskDataNotify.
+    /**
+     * Outputs taskVars as a proto as seen in DailyTaskDataNotify.
+     *
+     * @return A list of TaskVar protos with the current task vars.
+     */
     public List<TaskVar> getTaskVarsProto() {
         return this.taskVars.entrySet().stream().map(e -> {
             val taskVar = new TaskVar();
@@ -122,11 +155,22 @@ public class DailyTaskManager extends BasePlayerDataManager {
         }).toList();
     }
 
-    //outputs currentTasks as a proto as seen in WorldOwnerDailyTaskNotify.
+    /**
+     * Outputs currentTasks as a proto as seen in WorldOwnerDailyTaskNotify.
+     *
+     * @return A list of DailyTaskInfo protos with the current task info.
+     */
     public List<DailyTaskInfo> getTaskListProto() {
         return this.currentTasks.stream().map(this::getTaskInfoProto).toList();
     }
 
+    /**
+     * Converts a taskId into a DailyTaskInfo proto.
+     * Checks if it is finished or not.
+     *
+     * @param taskId The taskId that you want to turn into a proto.
+     * @return A list of DailyTaskInfo proto with the task info.
+     */
     public DailyTaskInfo getTaskInfoProto(int taskId) {
         val dailyTaskInfo = new DailyTaskInfo();
         dailyTaskInfo.setDailyTaskId(taskId);
@@ -145,47 +189,95 @@ public class DailyTaskManager extends BasePlayerDataManager {
         return dailyTaskInfo;
     }
 
+    /**
+     * Marks a taskId as having been finished today.
+     * Sends a DailyTaskProgressNotify and updates the PROP_PLAYER_LEGENDARY_DAILY_TASK_NUM player property.
+     *
+     * @param taskId The taskId that should be marked finished.
+     */
     public void finishTask(int taskId) {
         if (!finishedCurrentTasks.contains(taskId)) {
             finishedCurrentTasks.add(taskId);
-            LegendaryKeyDailyTasks += 1;
+            legendaryKeyDailyTasks += 1;
         }
 
         this.player.sendPacket(new PacketDailyTaskProgressNotify(getTaskInfoProto(taskId)));
-        this.player.setProperty(PlayerProperty.PROP_PLAYER_LEGENDARY_DAILY_TASK_NUM, LegendaryKeyDailyTasks);
+        this.player.setProperty(PlayerProperty.PROP_PLAYER_LEGENDARY_DAILY_TASK_NUM, legendaryKeyDailyTasks);
     }
 
+    /**
+     * Sets a task var to a value.
+     *
+     * @param taskId The taskId of the task var.
+     * @param index  The index for the value.
+     * @param value  The value of the task var.
+     */
     public void setTaskVar(int taskId, int index, int value) {
         getVarList(taskId, index).set(index, value);
         triggerTaskVarAction(taskId, index, value);
     }
 
+    /**
+     * Adds to the value of a task var and stores the result.
+     *
+     * @param taskId The taskId of the task var.
+     * @param index  The index for the value.
+     * @param value  The amount to add to the value of the task var.
+     */
     public void incTaskVar(int taskId, int index, int value) {
         val oldValue = getTaskVar(taskId, taskId);
         getVarList(taskId, index).set(index, oldValue + value);
         triggerTaskVarAction(taskId, index, oldValue + value);
     }
 
+    /**
+     * Subtracts from the value of a task var and stores the result.
+     *
+     * @param taskId The taskId of the task var.
+     * @param index  The index for the value.
+     * @param value  The amount to remove from the value of the task var.
+     */
     public void decTaskVar(int taskId, int index, int value) {
         incTaskVar(taskId, index, -value);
     }
 
+    /**
+     * Returns the value of the task var with a given taskId/index.
+     *
+     * @param taskId The taskId of the task var.
+     * @param index  The index for the value.
+     * @return The value of the task var with a given taskId/index.
+     */
     public int getTaskVar(int taskId, int index) {
         return getVarList(taskId, index).get(index);
     }
 
+    /**
+     * Returns a list of task var with a given taskId.
+     * Pads the list up to the given index.
+     *
+     * @param taskId The taskId of the task var.
+     * @param index  The padding needed to get the variable you want.
+     * @return A list of task var with a given taskId padded to the given index.
+     */
     private List<Integer> getVarList(int taskId, int index) {
         val list = this.taskVars.computeIfAbsent(taskId, k -> new ArrayList<>());
 
         //pad the list of variables with 0 entries in order to reach the index
         while (list.size() <= index) {
             list.add(0);
-
         }
 
         return list;
     }
 
+    /**
+     * Does the quest triggers and packets common to all task var operations.
+     *
+     * @param taskId The taskId of the task var.
+     * @param index  The index for the value.
+     * @param value  The value of the task var.
+     */
     private void triggerTaskVarAction(int taskId, int index, int value) {
         this.player.save();
         var questManager = this.player.getQuestManager();
